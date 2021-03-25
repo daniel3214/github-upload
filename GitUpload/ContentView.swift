d8d12abcf4982bbd1fd497057fef30143ceacda7
eckes <>
2021-03-25 22:16:00 +0100

    initializing repository

---
+++ GitUpload/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitUpload
+//
+//  Created by eckes on 25.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitUpload/GitUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitUploadApp.swift
+//  GitUpload
+//
+//  Created by eckes on 25.03.21.
+//
+
+import SwiftUI
+
+@main
+struct GitUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
